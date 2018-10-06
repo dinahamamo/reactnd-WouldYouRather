2af5d7b67eaf87757985fa9e33128c8c8b82b6dc
dinahamamo <dina.hamamo@gmail.com>
2018-10-06 11:47:14 +0200

    fix: 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,26 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './NotFound.css'
-import { withRouter, Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { setAuthedUser } from '../actions/authedUser';
+import { Link } from 'react-router-dom'
 
-class NotFound extends Component {
-  handleLogout = () => {
-    this.props.dispatch(setAuthedUser(null))
-    this.props.history.push("/")
-
-  }
-
-  render() {
-    return (
-      <div className="not-found">
-        <h1>OOPS</h1>
-        <h2>This page isn't here.</h2>
-        <Link to="/" onClick={this.handleLogout()}>Take me to the homepage!</Link>
-      </div>
-    )
-  }
-
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h1>OOPS</h1>
+      <h2>This page isn't here.</h2>
+      <Link to="/">Take me to the homepage!</Link>
+    </div>
+  )
 }
 
-export default withRouter(connect()(NotFound))
+export default NotFound
